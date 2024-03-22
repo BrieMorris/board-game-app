7366a2ca160a64abcab23ba442f09c85b5022582
Brie Morris <morris.brie@gmail.com>
2024-03-22 17:08:43 -0500

    set up nav bar

--- src/RecordGame/RecordGame.jsx
+++ src/RecordGame/RecordGame.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import {useSelector} from 'react-redux';
 
-//RecordGames function keeps track of the games user input 
+//RecordGames function keeps track of the games users input 
 function RecordGames(props) {
   // Using hooks we're creating local state for a "heading" variable with
   // a default value of 'Functional Component'
