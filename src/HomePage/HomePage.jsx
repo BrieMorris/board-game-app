c51cf1dead0db014f2c116216490232b17047c4e
Brie Morris <morris.brie@gmail.com>
2024-03-26 17:49:25 -0500

    home page

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
 import {useSelector} from 'react-redux';
 
-// Basic functional component structure for React with default state
-// value setup. When making a new component be sure to replace the
-// component name TemplateFunction with the name for the new component.
-function TemplateFunction(props) {
+//HomePage function is the landing page for users after login
+function HomePage(props) {
   // Using hooks we're creating local state for a "heading" variable with
   // a default value of 'Functional Component'
   const store = useSelector((store) => store);
@@ -18,4 +16,4 @@ function TemplateFunction(props) {
   );
 }
 
-export default TemplateFunction;
+export default HomePage;
