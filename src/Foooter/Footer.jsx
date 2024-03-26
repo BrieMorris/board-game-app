5bbbbfc63359cd45204d71fa9872ecafd90294ff
Brie Morris <morris.brie@gmail.com>
2024-03-25 19:48:11 -0500

    footer

---
+++ src/Foooter/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css';
+
+//simple footer function 
+
+function Footer() {
+  return <footer>&copy; Creator: Brie Morris</footer>;
+}
+
+export default Footer;
